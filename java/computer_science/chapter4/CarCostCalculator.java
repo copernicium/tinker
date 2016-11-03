a070cd25ac798eb30594c6b22184615dd8b00910
copernicium <logan.traffas@gmail.com>
2016-11-03 09:56:31 -0700

    Triangles and cars and strings oh my

--- java/computer_science/chapter4/CarCostCalculator.java
+++ java/computer_science/chapter4/CarCostCalculator.java
@@ -10,26 +10,74 @@ import java.util.Scanner;
  * assignment: Chapter 04--Fundamental Data Types--E4.10
  */
 public class CarCostCalculator {
-	public double getNumberOfGallons(){
+	private double numberOfGallons;//gallons
+	private double fuelEfficiency;//miles per gallon
+	private double gasPrice;//dollars per gallon
+
+	/**
+	 * Prompts the use to input the number of gallons of gas in their car and saves it
+	 */
+	public void setNumberOfGallons(){
 		Scanner input = new Scanner(System.in);
 		System.out.print("Input the number of gallons of gas in your gas tank (gal): ");
 		double numberOfGallons = input.nextDouble();
 		System.out.print("\n");
-		return numberOfGallons;
+		this.numberOfGallons = numberOfGallons;
 	}
-	public double getFuelEfficiency(){
+	/**
+	 * Prompts the use to input the fuel efficiency of their car and saves it
+	 */
+	public void setFuelEfficiency(){
 		Scanner input = new Scanner(System.in);
 		System.out.print("Input the fuel efficiency of your car (mi/gal): ");
 		double fuelEfficiency = input.nextDouble();
 		System.out.print("\n");
-		return fuelEfficiency;
+		this.fuelEfficiency = fuelEfficiency;
 	}
-	public double getCostOfGas(){
+	/**
+	 * Prompts the use to input the cost of gas per mile and saves it
+	 */
+	public void setCostOfGas(){
 		Scanner input = new Scanner(System.in);
 		System.out.print("Input the cost of gas (dollars/gal): ");
 		double gasPrice = input.nextDouble();
 		System.out.print("\n");
-		return gasPrice;
+		this.gasPrice = gasPrice;
+	}
+
+	/**
+	 * Calculates the distance the user can drive based on their car's fuel efficiency and the amount of gas in their tank
+	 * @return the distance the car can drive
+	 */
+	public double calculateDistance(){
+		double distance = 0.0;
+		distance = numberOfGallons * fuelEfficiency;
+		return distance;
+	}
+
+	/**
+	 * Calculates the cost of driving 100 miles given the cost of gas and the fuel efficiency of the car
+	 * @return the cost of driving 100 miles
+	 */
+	public double calculateCost(){
+		final double MILES_DRIVEN = 100.0;//miles
+		double cost = (MILES_DRIVEN / fuelEfficiency) * gasPrice;
+		return cost;
+	}
+
+	public CarCostCalculator(){
+		numberOfGallons = 0;
+		fuelEfficiency = 0;
+		gasPrice = 0;
+	}
+
+	public static void main(String[] args){
+		CarCostCalculator carCostCalculator = new CarCostCalculator();
+		carCostCalculator.setNumberOfGallons();
+		carCostCalculator.setFuelEfficiency();
+		carCostCalculator.setCostOfGas();
+		System.out.println("Cost of driving 100 miles: " + carCostCalculator.calculateCost() + " dollars");
+		System.out.println("Number of miles able to drive: " + carCostCalculator.calculateDistance() + " miles");
 	}
 
 }
---
+++ java/computer_science/chapter4/Triangle.java
@@ -0,0 +1,96 @@
+package computer_science.chapter4;
+import java.util.Map;
+import java.util.Scanner;
+/**
+ * A class which helps calculates all of the information about a triangle given three points
+ *
+ * @author Logan Traffas
+ * @version 11/3/2016
+ * assignment: Chapter 04--Fundamental Data Types-P4.3
+ */
+public class Triangle {
+	public static class Point{
+		private double x;
+		private double y;
+		public double getX(){
+			return  x;
+		}
+		public double getY(){
+			return y;
+		}
+		public Point(){
+			x = 0;
+			y = 0;
+		}
+		public Point(double x,double y){
+			this.x = x;
+			this.y = y;
+		}
+	}
+	private Point one, two, three;//points
+	private double a, b, c;//sides
+	private double A, B, C;//angles
+
+	public Triangle(){
+		one = new Point();
+		two = new Point();
+		three = new Point();
+		a = 0;
+		b = 0;
+		c = 0;
+		A = 0;
+		B = 0;
+		C = 0;
+	}
+
+	public Point inputPoint(){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Input x coordinate: ");
+		double x = in.nextDouble();
+		System.out.print("Input y coordinate: ");
+		double y = in.nextDouble();
+		return new Point(x,y);
+	}
+
+	public double calculateLength(Point pt1, Point pt2){
+		return Math.sqrt(Math.pow((pt2.x-pt1.x),2) + Math.pow((pt2.y-pt2.y),2));
+	}
+
+	public void setLengthA(){
+		this.a = calculateLength(this.one,this.two);
+	}
+
+	public void setLengthB(){
+		this.b = calculateLength(this.two,this.three);
+	}
+
+	public void setLengthC(){
+		this.c = calculateLength(this.three,this.one);
+	}
+
+	public double calculatePermiter(){
+		return this.a + this.b + this.c;
+	}
+
+	public double calculateArea(){
+		double s = (a + b + c)/2.0;
+		return Math.sqrt(s(s - a)(s - b)(s - c));
+	}
+
+	public void setPointOne(Point one){
+		this.one = one;
+	}
+
+	public void setPointTwo(Point two){
+		this.two = two;
+	}
+
+
+	public void setPointThree(Point three){
+		this.three = three;
+	}
+
+	public static void main(String[] args){
+		Triangle triangle = new Triangle();
+	}
+}
