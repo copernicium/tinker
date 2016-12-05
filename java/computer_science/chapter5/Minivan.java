1dd60f822c2f8f7b337abe7f35d5f7bc5d52b8f9
copernicium <logan.traffas@gmail.com>
2016-12-05 11:26:07 -0800

    Add more chapter five files

---
+++ java/computer_science/chapter5/Minivan.java
@@ -0,0 +1,36 @@
+package computer_science.chapter5;
+
+/**
+ * Simulated the minivan door opening mechanisms
+ *
+ * @author Logan Traffas
+ * @version 12/5/2016
+ * assignment: Chapter 05--Decisions--P5.14 Minivan Sliding Doors
+ */
+public class Minivan {
+	/*
+	"A minivan has two sliding doors. Each door can be opened by
+	either a dashboard switch, its inside handle, or its outside
+	handle. However, the inside handles do not work if a child
+	lock switch is activated. In order for the sliding doors to open,
+	the gear shift must be in park, and the master unlock switch
+	 must be activated. (This book’s author is the long­suffering
+	owner of just such a vehicle.)
+
+	Your task is to simulate a portion of the control software for
+	the vehicle. The input is a sequence of values for the switches
+	and the gear shift, in the following order:
+	•	Dashboard switches for left and right sliding door,
+	child lock, and master unlock (0 for off or 1 for activated)
+	•	Inside and outside handles on the left and right sliding doors (0 or 1)
+	• Thegearshiftsetting(oneofPND123R). Atypicalinputwouldbe 0 0 0 1 0 1 0 0 P.
+
+	Print “left door opens” and/or “right door opens” as appropriate. If neither door opens, print “both doors stay closed”."
+	 (See Horstmann p234-235)
+
+	Submit your code AND a sample run of your program showing all possible inputs/outputs
+	 */
+	public static void main(String[] args){
+
+	}
+}
---
+++ java/computer_science/chapter5/Seasons.java
@@ -0,0 +1,103 @@
+package computer_science.chapter5;
+
+import MySystem.MySystem;
+
+import java.util.Scanner;
+
+/**
+ * Prompts the user for a month and day and then prints the season
+ *
+ * @author Logan Traffas
+ * @version 12/5/2016
+ * assignment: Chapter 05--Decisions--E5.14--Seasons
+ */
+public class Seasons {
+	public static class Date {
+		private int month;
+		private int day;
+
+		public int getDay() {
+			return this.day;
+		}
+
+		public int getMonth() {
+			return this.month;
+		}
+
+		public Date() {
+			this.month = 0;
+			this.day = 0;
+		}
+
+		public Date(int month, int day) {
+			MySystem.myAssert(1 <= month && month <= 12,MySystem.getFileName(),MySystem.getLineNumber());
+			MySystem.myAssert(1 <= month && month <= 31,MySystem.getFileName(),MySystem.getLineNumber());
+
+			this.month = month;
+			this.day = day;
+		}
+	}
+	public enum Season{WINTER,SPRING,SUMMER,FALL}
+
+	Date date;
+
+	public void setDate(){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the month and day in number format (ex: May 9th is \"5 9\"): ");
+		String date = input.nextLine().trim();
+
+		String monthStr = "", dayStr = "";
+
+		{
+			boolean addToMonth = true;
+			for(char a : date.toCharArray()) {
+				if(a == ' ') {
+					addToMonth  = false;
+					continue;
+				}
+				if(addToMonth ) monthStr += a;
+				else dayStr += a;
+			}
+
+			monthStr = monthStr.trim();
+			dayStr = dayStr.trim();
+		}
+		int month = Integer.parseInt(monthStr);
+		int day = Integer.parseInt(dayStr);
+		this.date = new Date(month,day);
+	}
+
+	public Season getSeason() {
+		Season season;
+		if(1 <= this.date.getMonth() && this.date.getMonth() <= 3) {
+			season = Season.WINTER;
+		} else if(4 <= this.date.getMonth() && this.date.getMonth() <= 6) {
+			season = Season.SPRING;
+		} else if(7 <= this.date.getMonth() && this.date.getMonth() <= 9) {
+			season = Season.SUMMER;
+		} else season = Season.FALL;
+
+		boolean advance = (this.date.getMonth() % 3 == 0 && this.date.getDay() >= 21);
+		if(season == Season.WINTER && advance) {
+			season = Season.SPRING;
+		} else if(season == Season.SPRING && advance) {
+			season = Season.SUMMER;
+		} else if(season == Season.SUMMER && advance) {
+			season = Season.FALL;
+		} else if(season == Season.FALL && advance){
+			season = Season.WINTER;
+		}
+		return season;
+	}
+
+	public Seasons() {
+		this.date = new Date();
+	}
+
+	public static void main(String[] args){
+		Seasons seasons = new Seasons();
+		seasons.setDate();
+		System.out.println("The season is " + seasons.getSeason());
+	}
+
+}
