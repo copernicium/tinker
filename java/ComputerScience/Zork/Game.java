31d905c29f80a4a4c05dee2b9902d90de11c34fe
copernicium <logan.traffas@gmail.com>
2017-02-13 11:27:48 -0800

    More work on zork

--- java/ComputerScience/Zork/Game.java
+++ java/ComputerScience/Zork/Game.java
@@ -1,9 +1,46 @@
 package ComputerScience.Zork;
 
+import java.util.ArrayList;
+
 /**
- *
+ * Runs the Zork Game
  */
 public class Game {
+	private ArrayList<Treasure> inventory;
+	private ArrayList<Room> rooms;
+	private ArrayList<Treasure> treasures;
+
+	/**
+	 * Constructs the Game object
+	 * Create the rooms and add to the list
+	 * Create exits and add to the list
+	 */
+	public Game(){
+		this.inventory = new ArrayList<>();
+		this.treasures = new ArrayList<>();
+		this.rooms = new ArrayList<>();
+		/*Room gym = new Room("Gym", "1");
+		Room pit = new Room("Pit", "2");
+		Room cafe = new Room("Cafe", "3");
+		Room cafeteria = new Room("Cafeteria", "4");
+		Room theShed = new Room("The Shed", "5");
+		Room theRoom = new Room("The Room", "6");
+		this.rooms.add(gym);
+		this.rooms.add(pit);
+		this.rooms.add(cafe);
+		this.rooms.add(cafeteria);
+		this.rooms.add(theShed);
+		this.rooms.add(theRoom);*/
+		Room livingRoom = new Room("Living Room","");
+		Room kitchen = new Room("Kitchen","");
+		Room bathroom = new Room("Bathroom","");
+		Room porch = new Room("Porch","");
+		Treasure kitten = new Treasure("Kitten",livingRoom);
+		Treasure knife = new Treasure("Knife",kitchen);
+		this.treasures.add(kitten);
+		this.treasures.add(knife);
+
+	}
 
 	public static void main(String[] args){
 
--- java/ComputerScience/Zork/Treasure.java
+++ java/ComputerScience/Zork/Treasure.java
@@ -1,16 +1,27 @@
 package ComputerScience.Zork;
 
 /**
- *
+ * Stores the treasure for a room
  */
 public class Treasure {
 	String name;
+	Room room;
 
+	/**
+	 * Returns the name of the treasure
+	 * @return the treasure's name
+	 */
 	public String getName(){
 		return this.name;
 	}
 
-	public Treasure(String name){
+	/**
+	 * Contruct a Treasure object
+	 * @param name the name of the treasure
+	 * @param room the room that the treasure is in
+	 */
+	public Treasure(String name, Room room){
 		this.name = name;
+		this.room = room;
 	}
 }
