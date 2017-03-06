0ac3132a1df7b4dffe7b009399afa7b372371430
High School Computer <donotreply@schoolcomputer.com>
2017-03-06 10:55:14 -0800

    Oops, forgot cpp file

--- cpp/simple_time.h
+++ cpp/simple_time.h
@@ -1,11 +1,24 @@
 #include <chrono>
+#include <iostream>
 #include <cassert>
 
 namespace Simple_time{
 
-	enum class Unit{SECONDS,MILLISECONDS,MICROSECONDS,NANOSECONDS};
+	#define UNITS \
+		X(SECONDS,seconds) \
+		X(MILLISECONDS,milliseconds) \
+		X(MICROSECONDS,microseconds) \
+		X(NANOSECONDS,nanoseconds)
+			
+	enum class Unit{
+		#define X(UNIT,B) UNIT,
+		UNITS
+		#undef X
+	};
 	
-	void sleep(int,Unit);
+	void sleep(long int,Unit);
 	
 	unsigned long int get_time(Unit);
 }
+
+std::ostream& operator<<(std::ostream&,Simple_time::Unit const&);
