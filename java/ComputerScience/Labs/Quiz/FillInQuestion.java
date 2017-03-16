5eabeca4b1e150750c7d859e1a506b5a757276f4
copernicium <logan.traffas@gmail.com>
2017-03-16 11:27:49 -0700

    Begin working on quiz classes

---
+++ java/ComputerScience/Labs/Quiz/FillInQuestion.java
@@ -0,0 +1,13 @@
+package ComputerScience.Labs.Quiz;
+
+/**
+ * @Author Logan Traffas
+ * @Date 3/16/2017.
+ * @Version 1.0.0
+ * @Assignment Ch 9 Lab: Quiz Maker
+ */
+public class FillInQuestion extends Question{
+	public FillInQuestion(){
+		super();
+	}
+}
