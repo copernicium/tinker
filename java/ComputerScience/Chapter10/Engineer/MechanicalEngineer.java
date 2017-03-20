a3e107916afbe35996c35aacf17d671b97895cb9
copernicium <logan.traffas@gmail.com>
2017-03-20 10:38:55 -0700

    Add engineer files

---
+++ java/ComputerScience/Chapter10/Engineer/MechanicalEngineer.java
@@ -0,0 +1,14 @@
+package ComputerScience.Chapter10.Engineer;
+
+/**
+ * @Author Logan Traffas
+ * @Date 3/20/2017.
+ * @Version 1.0.0
+ * @Assignment Ch10-Exercise1-Java Interface Tutorials--Engineer Interface
+ */
+public class MechanicalEngineer implements Engineer {
+	public void useCAD(){}
+	public void designProduct(){}
+	public void payDues(){}
+	public void getTitle(){}
+}
