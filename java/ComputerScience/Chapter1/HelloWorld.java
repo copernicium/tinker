178727ca1c56e2392c8f38e87f0e3773333737e4
Logan <logan.traffas@gmail.com>
2017-06-10 02:50:54 -0700

    Remove AP Computer Science files (moved to separate respository)

--- java/ComputerScience/Chapter1/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-package ComputerScience.Chapter1;
-public class HelloWorld{
-   public static void main(String[] args){
-       System.out.println("Hello my fellow inmates");
-    }
-}
--- java/ComputerScience/Chapter10/Animals/Speakable.java
+++
@@ -1,8 +0,0 @@
-package ComputerScience.Chapter10.Animals;
-
-/**
- * Created by Logan on 3/29/2017.
- */
-public interface Speakable{
-	void speak();
-}
