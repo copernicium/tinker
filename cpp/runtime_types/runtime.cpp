4f3e4424370e728e3866111fdc088dde63729171
Logan <logan.traffas@gmail.com>
2018-10-28 00:42:45 -0700

    Add container types

--- cpp/runtime_types/runtime.cpp
+++ cpp/runtime_types/runtime.cpp
@@ -1,24 +1,78 @@
 #include "runtime.h"
 #include <iostream>
 
-// Int a = 5
-// Int b = 5
+// Int a = TEST_VAL
+// Int b = TEST_VAL
 // Int c = a + b
 
+ObjectBase::Type toType(const std::string& IN){
+	if(IN == "Real"){
+		return ObjectBase::Type::REAL;
+	} else if(IN == "Int"){
+		return ObjectBase::Type::INT;
+	}
+	assert(0);
+}
+
 int main(){
+	constexpr unsigned TEST_VAL = 5;
+
 	std::string type_str;
 	std::cout<<"Type: ";
 	std::cin>>type_str;
-	
-	ObjectBase::Type type = (type_str == "Real") ? ObjectBase::Type::REAL : ObjectBase::Type::INT;
-	
-	std::shared_ptr<ObjectBase> a = create(type);
-	set(a, 5);
-	
-	std::shared_ptr<ObjectBase> b = create(type);
-	set(b, 5);
 
-	std::shared_ptr<ObjectBase> c = visit(a, b, add);
-	
-	std::cout<<c->toString()<<"\n";
+	ObjectBase::Type type = toType(type_str);
+
+	{
+		std::shared_ptr<ObjectBase> a = create(type);
+		visit(a, assign, TEST_VAL);
+
+		std::shared_ptr<ObjectBase> b = create(type);
+		visit(b, assign, TEST_VAL);
+
+		std::shared_ptr<ObjectBase> c = visit(a, b, add);
+
+		std::cout<<c->toString()<<"\n";
+	}
+	{
+		int size = 0, index = 0;
+
+		std::cout<<"Type: ";
+		std::cin>>type_str;
+		type = toType(type_str);
+
+		std::cout<<"Size: ";
+		std::cin>>size;
+
+		std::cout<<"Test index: ";
+		std::cin>>index;
+
+		std::shared_ptr<ObjectBase> list = create(create(type), size);
+		visit(at(list, index), assign, TEST_VAL);
+
+		std::cout<<list->toString()<<"\n";
+		std::cout<<at(list, index)->toString()<<"\n";
+	}
+	{
+		std::vector<std::shared_ptr<ObjectBase>> types;
+		int index = 0;
+
+		while(true){
+			std::cout<<"Type: ";
+			std::cin>>type_str;
+			if(type_str == "q"){
+				break;
+			}
+			types.push_back(create(toType(type_str)));
+		}
+
+		std::cout<<"Test index: ";
+		std::cin>>index;
+
+		std::shared_ptr<ObjectBase> collection = create(types);
+		visit(at(collection, index), assign, TEST_VAL);
+
+		std::cout<<collection->toString()<<"\n";
+		std::cout<<at(collection, index)->toString()<<"\n";
+	}
 }
--- cpp/runtime_types/runtime.h
+++ cpp/runtime_types/runtime.h
@@ -1,10 +1,14 @@
 #include <cassert>
+#include <vector>
 #include <memory>
 
-// Object types
-
 struct ObjectBase{
-	enum class Type{INT, REAL};
+	enum class Type{
+		INT,
+		REAL,
+		LIST,
+		COLLECTION
+	};
 
 protected:
 	Type type;
@@ -13,9 +17,13 @@ public:
 	Type getType()const noexcept{
 		return type;
 	}
-	
+
+	ObjectBase() = delete;
+
 	ObjectBase(const Type& t)noexcept: type(t){}
-	
+
+	virtual std::shared_ptr<ObjectBase> clone()const noexcept = 0;
+
 	virtual std::string toString()const noexcept = 0;
 };
 
@@ -28,65 +36,153 @@ public:
 	T getValue()const noexcept{
 		return value;
 	}
-	
-	void setValue(T a)noexcept{
+
+	void setValue(const T& a)noexcept{
 		value = a;
 	}
-	
+
 	std::string toString()const noexcept{
-		return "Object(" + std::to_string(value) + ")";
+		return std::to_string(value);
 	}
-	
+
+	std::shared_ptr<ObjectBase> clone()const noexcept{
+		return std::make_shared<Object<T, ObjectType>>(*this);
+	}
+
 	Object(T v)noexcept: ObjectBase(ObjectType), value(v){}
+
 	Object()noexcept: ObjectBase(ObjectType), value(T()){}
 };
 
 using Int = Object<int,ObjectBase::Type::INT>;
 using Real = Object<double, ObjectBase::Type::REAL>;
 
-// Visitors
-
 struct Add{
 	template<typename T>
-	std::shared_ptr<ObjectBase> operator()(T a, T b)const noexcept{
+	std::shared_ptr<ObjectBase> operator()(const T& a, const T& b)const noexcept{
 		return std::make_shared<T>(T(a.getValue() + b.getValue()));
 	}
 };
-static constexpr Add add;
 
-template<typename F>
-std::shared_ptr<ObjectBase> visit(std::shared_ptr<ObjectBase> a, std::shared_ptr<ObjectBase> b, F op){
+constexpr Add add;
+
+template<typename F, typename... Args>
+std::shared_ptr<ObjectBase> visit(const std::shared_ptr<ObjectBase>& a, const std::shared_ptr<ObjectBase>& b, const F& f, const Args&... args){
 	assert(a->getType() == b->getType());
+
 	switch(a->getType()){
 	case ObjectBase::Type::REAL:
-		return op(*std::dynamic_pointer_cast<Real>(a), *std::dynamic_pointer_cast<Real>(b));
+		return f(*std::dynamic_pointer_cast<Real>(a), *std::dynamic_pointer_cast<Real>(b), args...);
 	case ObjectBase::Type::INT:
-		return op(*std::dynamic_pointer_cast<Int>(a), *std::dynamic_pointer_cast<Int>(b));
+		return f(*std::dynamic_pointer_cast<Int>(a), *std::dynamic_pointer_cast<Int>(b), args...);
 	default:
 		assert(0);
 	}
 }
 
-template<typename T>
-void set(std::shared_ptr<ObjectBase> a, T value){
+struct Assign{
+	template<typename T, typename R>
+	void operator()(const T& a, const R& b)const noexcept{
+		a->setValue(b);
+	}
+};
+
+constexpr Assign assign;
+
+template<typename F, typename... Args>
+void visit(const std::shared_ptr<ObjectBase>& a, const F& f, const Args&... args){
 	switch(a->getType()){
 	case ObjectBase::Type::REAL:
-		std::dynamic_pointer_cast<Real>(a)->setValue(value);
+		f(std::dynamic_pointer_cast<Real>(a), args...);
 		break;
 	case ObjectBase::Type::INT:
-		std::dynamic_pointer_cast<Int>(a)->setValue(value);
+		f(std::dynamic_pointer_cast<Int>(a), args...);
 		break;
 	default:
 		assert(0);
 	}
 }
-	
+
 std::shared_ptr<ObjectBase> create(const ObjectBase::Type& TYPE){
 	switch(TYPE){
 	case ObjectBase::Type::REAL:
 		return std::make_shared<Real>(Real());
 	case ObjectBase::Type::INT:
 		return std::make_shared<Int>(Int());
+	default:
+		assert(0);
+	}
+}
+
+struct Container: public ObjectBase{
+protected:
+	std::vector<std::shared_ptr<ObjectBase>> inner;
+
+	Container(const ObjectBase::Type& TYPE): ObjectBase(TYPE){}
+
+public:
+	std::shared_ptr<ObjectBase>& at(const std::size_t& I)noexcept{
+		return inner.at(I);
+	}
+
+	const std::shared_ptr<ObjectBase>& at(const std::size_t& I)const noexcept{
+		return inner.at(I);
+	}
+
+	std::size_t size()const noexcept{
+		return inner.size();
+	}
+
+	std::string toString()const noexcept{
+		std::string s = "[";
+		for(const auto& a: inner){
+			s += a->toString() + " ";
+		}
+		s += "]";
+		return s;
+	}
+};
+
+struct List: public Container{
+	std::shared_ptr<ObjectBase> clone()const noexcept{
+		return std::make_shared<List>(*this);
+	}
+
+	List(const std::shared_ptr<ObjectBase>& TYPE, const std::size_t& SIZE)noexcept: Container(ObjectBase::Type::LIST){
+		this->inner.reserve(SIZE);
+		for(unsigned i = 0; i < SIZE; i++){
+			inner.push_back(TYPE->clone());
+		}
+	}
+};
+
+std::shared_ptr<ObjectBase> create(const std::shared_ptr<ObjectBase>& TYPE, const std::size_t& SIZE){
+	return std::make_shared<List>(List(TYPE, SIZE));
+}
+
+struct Collection: public Container{
+	std::shared_ptr<ObjectBase> clone()const noexcept{
+		return std::make_shared<Collection>(*this);
+	}
+
+	Collection(const std::vector<std::shared_ptr<ObjectBase>>& TYPES)noexcept: Container(ObjectBase::Type::COLLECTION){
+		this->inner.reserve(TYPES.size());
+		for(unsigned i = 0; i < TYPES.size(); i++){
+			inner.push_back(TYPES[i]->clone());
+		}
+	}
+};
+
+std::shared_ptr<ObjectBase> create(const std::vector<std::shared_ptr<ObjectBase>>& TYPES){
+	return std::make_shared<Collection>(Collection(TYPES));
+}
+
+std::shared_ptr<ObjectBase> at(const std::shared_ptr<ObjectBase>& CONTAINER, const std::size_t& I){
+	switch(CONTAINER->getType()){
+	case ObjectBase::Type::LIST:
+		return std::dynamic_pointer_cast<List>(CONTAINER)->at(I);
+	case ObjectBase::Type::COLLECTION:
+		return std::dynamic_pointer_cast<Collection>(CONTAINER)->at(I);
 	default:
 		assert(0);
 	}
